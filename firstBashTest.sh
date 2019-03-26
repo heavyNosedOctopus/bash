3f066218bffe02a988ac923f59243ce04a59f62a
André Roberto Figueiró de Magalhães <arfddem@gmail.com>
2019-03-26 19:03:10 -0300

    Inicialização

---
+++ firstBashTest.sh
@@ -0,0 +1,27 @@
+printf 'Oi!\n'
+sleep 3s
+printf 'Entre:\n'
+sleep 1s
+printf 'Cavalos\n'
+sleep 1s
+printf 'Aranhas\n'
+sleep 1s
+printf 'Gatos\n'
+sleep 2s
+printf 'O que você prefere?\n'
+
+read resposta
+
+case $resposta in
+    Cavalos|cavalos)
+        echo "Eu não."
+        ;;
+    Aranhas|aranhas)
+        echo "Elas são daora mesmo."
+        ;;
+    Gatos|gatos)
+        echo "Me too."
+        ;;
+    *)
+        echo "Lê direitinho na próxima."
+esac
